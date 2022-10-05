83fd5511ebd02be203e152b6e1e68d7b7242e2f7
Romanets Pavel <pavelroma24@gmail.com>
2022-10-05 18:30:39 +0300

    add: admin layouts and MainControllers for admin and site

---
+++ resources/views/admin/users/login.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Вход в аккаунт</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('assets/admin/css/admin.css') }}">
+</head>
+<body class="hold-transition register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <b>Вход в аккаунт</b>
+        </div>
+
+        <div class="card">
+            <div class="card-body register-card-body">
+
+                @include('admin.layouts.message')
+
+                <form action="{{ route('users.login') }}" method="post">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control" placeholder="Почта"
+                               value="{{ old('email') }}">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control" placeholder="Пароль">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col-4 offset-8">
+                            <button type="submit" class="btn btn-primary btn-block">Войти</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
+    </div>
+    <!-- /.register-box -->
+{{--<script src="{{ asset('assets/admin/js/admin.js') }}"></script>--}}
+</body>
+</html>
