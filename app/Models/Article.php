57b1c90428eae33700bc1e86332b95093d2e67c4
Romanets Pavel <pavelroma24@gmail.com>
2022-10-05 22:21:20 +0300

    up: add uploadImage and getImage functions

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class Article extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'content', 'image'];
+
+    public static function uploadImage(Request $request, $image = null)
+    {
+        if ($request->hasFile('image')) {
+            if ($image) {
+                Storage::delete($image);
+            }
+            $folder = date('Y-m-d');
+            return $request->file('image')->store("{$folder}");
+        }
+        return null;
+    }
+
+    public function getImage()
+    {
+        if (str_contains($this->image, 'http')) {
+            return asset($this->image);
+        }
+
+        return asset("public/storage/{$this->image}");
+    }
 }
