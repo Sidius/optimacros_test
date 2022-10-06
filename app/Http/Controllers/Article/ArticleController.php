3d394a852a11c0154a7d31775df319af4ea086d5
Romanets Pavel <pavelroma24@gmail.com>
2022-10-06 11:24:57 +0300

    add: single page of article fix: paths in navbar, layout

---
+++ app/Http/Controllers/Article/ArticleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Article;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::query()->orderBy('created_at', 'desc')->paginate(3);
+
+        return view('articles.index', compact('articles'));
+    }
+
+    public function single($id)
+    {
+        $article = Article::query()->findOrFail($id);
+
+        return view('articles.single', compact('article'));
+    }
+}
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -53,7 +53,7 @@
                             <div class="list-group">
                                 <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
                                     <div class="w-100 justify-content-between">
-                                        <img src="public/assets/front/upload/small_04.jpg" alt="" class="img-fluid float-left">
+                                        <img src="{{ asset('public/assets/front/upload/small_04.jpg') }}" alt="" class="img-fluid float-left">
                                         <h5 class="mb-1">5 Beautiful buildings you need to before dying</h5>
                                         <small>12 Jan, 2016</small>
                                     </div>
@@ -61,7 +61,7 @@
 
                                 <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
                                     <div class="w-100 justify-content-between">
-                                        <img src="public/assets/front/upload/small_05.jpg" alt="" class="img-fluid float-left">
+                                        <img src="{{ asset('public/assets/front/upload/small_05.jpg') }}" alt="" class="img-fluid float-left">
                                         <h5 class="mb-1">Let's make an introduction for creative life</h5>
                                         <small>11 Jan, 2016</small>
                                     </div>
@@ -69,7 +69,7 @@
 
                                 <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
                                     <div class="w-100 last-item justify-content-between">
-                                        <img src="public/assets/front/upload/small_06.jpg" alt="" class="img-fluid float-left">
+                                        <img src="{{ asset('public/assets/front/upload/small_06.jpg') }}" alt="" class="img-fluid float-left">
                                         <h5 class="mb-1">Did you see the most beautiful sea in the world?</h5>
                                         <small>07 Jan, 2016</small>
                                     </div>
@@ -86,7 +86,7 @@
                             <div class="list-group">
                                 <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
                                     <div class="w-100 justify-content-between">
-                                        <img src="public/assets/front/upload/small_01.jpg" alt="" class="img-fluid float-left">
+                                        <img src="{{ asset('public/assets/front/upload/small_01.jpg') }}" alt="" class="img-fluid float-left">
                                         <h5 class="mb-1">Banana-chip chocolate cake recipe with customs</h5>
                                         <span class="rating">
                                                 <i class="fa fa-star"></i>
@@ -100,7 +100,7 @@
 
                                 <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
                                     <div class="w-100 justify-content-between">
-                                        <img src="public/assets/front/upload/small_02.jpg" alt="" class="img-fluid float-left">
+                                        <img src="{{ asset('public/assets/front/upload/small_02.jpg') }}" alt="" class="img-fluid float-left">
                                         <h5 class="mb-1">10 practical ways to choose organic vegetables</h5>
                                         <span class="rating">
                                                 <i class="fa fa-star"></i>
@@ -114,7 +114,7 @@
 
                                 <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
                                     <div class="w-100 last-item justify-content-between">
-                                        <img src="public/assets/front/upload/small_03.jpg" alt="" class="img-fluid float-left">
+                                        <img src="{{ asset('public/assets/front/upload/small_03.jpg') }}" alt="" class="img-fluid float-left">
                                         <h5 class="mb-1">We are making homemade ravioli, nice and good</h5>
                                         <span class="rating">
                                                 <i class="fa fa-star"></i>
